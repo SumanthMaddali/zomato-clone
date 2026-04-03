556193e338d4523a54bce53a071d12df078a3f01
Sai Sumanth Maddali <93214773+SumanthMaddali@users.noreply.github.com>
2026-04-04 00:29:30 +0530

    Change 'Log in' text to 'Sign in' in Header
    Making the change to test the github webhook trigger in jenkins

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <div className="right">
           <span>Investor Relations</span>
           <span>Add restaurant</span>
-          <span>Log in</span>
+          <span>Sign in</span>
           <span>Sign up</span>
         </div>
       </nav>
